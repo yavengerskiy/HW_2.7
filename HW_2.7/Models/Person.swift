13c6d5a41e49200f126fad5f02a4ecb3c75b3b2b
YaVengerskiy <yavengerskiy@yandex.ru>
2021-11-09 10:51:38 +0500

    first commit

---
+++ HW_2.7/Models/Person.swift
@@ -0,0 +1,13 @@
+//
+//  Person.swift
+//  HW_2.7
+//
+//  Created by Beelab_ on 09/11/21.
+//
+
+struct Person {
+    let firstName: String
+    let lastName: String
+    let phoneNumber: String
+    let email: String
+}
---
+++ HW_2.7/Services/DataManager.swift
@@ -0,0 +1,49 @@
+//
+//  DataManager.swift
+//  HW_2.7
+//
+//  Created by Beelab_ on 09/11/21.
+//
+
+class DataManager {
+    let firstName = ["firstName0",
+                     "firstName1",
+                     "firstName2",
+                     "firstName3",
+                     "firstName4",
+                     "firstName5",
+                     "firstName6",
+                     "firstName7",
+                     "firstName8",
+                     "firstName9"]
+    let lastName = ["lastName0",
+                    "lastName1",
+                    "lastName2",
+                    "lastName3",
+                    "lastName4",
+                    "lastName5",
+                    "lastName6",
+                    "lastName7",
+                    "lastName8",
+                    "lastName9"]
+    let phoneNumber = ["123456789",
+                       "123456789",
+                       "123456789",
+                       "123456789",
+                       "123456789",
+                       "123456789",
+                       "123456789",
+                       "123456789",
+                       "123456789",
+                       "123456789"]
+    let email = ["firstName0@gmail.com",
+                 "firstName1@gmail.com",
+                 "firstName2@gmail.com",
+                 "firstName3@gmail.com",
+                 "firstName4@gmail.com",
+                 "firstName5@gmail.com",
+                 "firstName6@gmail.com",
+                 "firstName7@gmail.com",
+                 "firstName8@gmail.com",
+                 "firstName9@gmail.com"]
+}
